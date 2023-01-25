e91c25c84b7e302036673880f28d40161e7c8a50
yis-park <ckadltmf423@naver.com>
2023-01-25 17:52:47 +0900

    MainIntro진행중

--- src/components/main/MainIntro.js
+++ src/components/main/MainIntro.js
@@ -1,10 +1,19 @@
-import React from "react";
+import "../style/mainIntro.scss";
+import "../../assets/images/scroll-down.jpg";
 
 function MainIntro(props) {
   return (
-    <div>
-      <h1> 인트로</h1>
-    </div>
+    <main className="intro">
+      <h1>Slow and steady</h1>
+      <h3>Front-end Developer</h3>
+      <h3>YiSeul-Park</h3>
+      <h3>PortFolio</h3>
+
+      <div>
+        <img src="/scroll-down.jpg" alt="scrolldown" />
+        <h4>Folio-23</h4>
+      </div>
+    </main>
   );
 }
 
